51588a005911fa84ceb33a8f7cfbcb1aa664db25
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-06-21 22:50:36 +0400

    city info commit

---
+++ City Projects/City_Info.js
@@ -0,0 +1,49 @@
+function RedirectLiverpool() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "Liverpool is a city and metropolitan borough in Merseyside, northwest England. It had a population of 496,770 in 2022.[3] The city is located on the eastern side of the Mersey Estuary, adjacent to the Irish Sea, and is approximately 178 miles (286 km) from London. Liverpool is the fifth largest city in the United Kingdom, the largest settlement in Merseyside and part of the Liverpool City Region, a combined authority with a population of over 1.5 million" 
+}
+
+function RedirectTbilisi() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "Because of its location at the crossroads between Europe and Asia, and its proximity to the lucrative Silk Road, throughout history, Tbilisi has been a point of contention among various global powers. To this day, the city's location ensures its position as an important transit route for energy and trade projects. Tbilisi's history is reflected in its architecture, which is a mix of medieval, neoclassical, Beaux Arts, Art Nouveau, Stalinist, and Modern structures." 
+}
+
+function RedirectSantorini() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "The island was the site of one of the largest volcanic eruptions in recorded history: the Minoan eruption (sometimes called the Thera eruption), which occurred about 3,600 years ago at the height of the Minoan civilization.[5] The eruption left a large caldera surrounded by volcanic ash deposits hundreds of metres deep."
+}
+
+function RedirectCapeTown() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "Cape Town[a] is the legislative capital of South Africa. It is the country's oldest city and the seat of the Parliament of South Africa.[11] It is the country's second-largest city, after Johannesburg, and the largest in the Western Cape.[12] The city is part of the City of Cape Town metropolitan municipality."
+}
+
+function RedirectDubrovnik() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "The history of the city probably dates back to the 7th century, when the town known as Ragusa was founded by refugees from Epidaurum (Ragusa Vecchia). It was under protectorate of the Byzantine Empire and later under the sovereignty of the Republic of Venice. Between the 14th and 19th centuries, Dubrovnik ruled itself as a free state."
+}
+
+function RedirectTokyo() {
+    let Original = document.getElementById("OGContent")
+    let ScrollTo = document.getElementById("OGContent")
+    ScrollTo.scrollIntoView({behavior: 'smooth'})
+    Original.innerHTML = "Located at the head of Tokyo Bay, Tokyo is part of the Kantō region on the central coast of Honshu, Japan's largest island. Tokyo serves as Japan's economic center and the seat of both the Japanese government and the Emperor of Japan. The Tokyo Metropolitan Government administers Tokyo's central 23 special wards (which formerly made up Tokyo City)."
+}
+
+function Return() {
+    let Return = document.getElementById("ToGrid")
+    Return.scrollIntoView({behavior: 'smooth'})
+}
+
+
+
